ae255ba84c15f221e9224a4a6c3ad9c763bfc1c1
Daniel Garnier-Moiroux <dgarnier-moiroux@pivotal.io>
2018-08-01 14:43:20 +0200

    Measure load time of X events and print it

--- src/main/kotlin/wf/garnier/spring/hibernatefun/HibernateFunApplication.kt
+++ src/main/kotlin/wf/garnier/spring/hibernatefun/HibernateFunApplication.kt
@@ -2,13 +2,17 @@ package wf.garnier.spring.hibernatefun
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import kotlin.system.measureTimeMillis
 
 @SpringBootApplication
 class HibernateFunApplication(eventRepo: EventRepository) {
 
     init {
-        val tenEvents = eventRepo.findAll().take(10)
-        println(tenEvents)
+        val initTime = measureTimeMillis {
+            val tenEvents = eventRepo.findAll().take(10)
+            tenEvents.forEach(System.out::println)
+        }
+        println("Load finished, in ${initTime}ms !")
     }
 }
 
